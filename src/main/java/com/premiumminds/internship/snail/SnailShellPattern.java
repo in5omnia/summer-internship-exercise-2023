bae1c4368d80312513e498e97960b992631bb5be
in5omnia <93283175+in5omnia@users.noreply.github.com>
2023-06-09 17:52:23 +0100

    maybe last Version

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -17,53 +17,41 @@ class SnailShellPattern implements ISnailShellPattern {
    * @param matrix matrix of numbers to go through
    * @return order array of values thar represent a snail shell pattern
    */
-  public Future<int[]> getSnailShell(final int[][] matrix) {
-    //create executor
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
-    //create callable
-    Callable<int[]> callable = new Callable<int[]>() {
-      @Override
-      public int[] call() throws Exception {
+    public Future<int[]> getSnailShell(final int[][] matrix) {
+        //create executor
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        //create callable
+        Callable<int[]> callable = new Callable<int[]>() {
+              @Override
+              public int[] call() {
+                  int col, row, N = matrix[0].length;
+                  //empty matrix 0x0
+                  if (N == 0) return new int[0];
+                  //matrix 1x1
+                  if (N == 1) return new int[]{matrix[0][0]};
 
-          int col, row, N = matrix[0].length;
-          //empty matrix 0x0
-          if (N == 0) return new int[0];
-          //matrix 1x1
-          if (N == 1) return new int[]{matrix[0][0]};
+                  int[] res = new int[N*N];
+                  int index = 0, startPos = 0, lastPos = N-1;   // because always startRow = startCol and lastRow = lastCol
 
-
-          int[] res = new int[N*N];
-          int index = 0, startPos = 0, lastPos = N-1;   // because always startRow = startCol and lastRow = lastCol
-
-          while (startPos < lastPos){
-
-              //get first Row
-              for (col = startPos; col <= lastPos - 1; col++, index++) res[index] = matrix[startPos][col];
-
-              // get last Col
-              for (row = startPos; row <= lastPos - 1; row++, index++) res[index] = matrix[row][lastPos];
-
-              //get last Row inverted
-              for (col = lastPos; col >= startPos + 1; col--, index++) res[index] = matrix[lastPos][col];
-
-              //get first Col inverted
-              for (row = lastPos; row >= startPos + 1; row--, index++) res[index] = matrix[row][startPos];
-
-              ++startPos;
-              --lastPos;
-
-
-          }
-          if (!(N % 2 == 0)) res[index] = matrix[startPos][startPos];   // if N isn't an even number
-          return res;
-      }
-    };
-    //create future variable
-    Future<int[]> future = executorService.submit(callable);
-
-    //return future
-    return future;
-  }
+                  while (startPos < lastPos){
+                      //get first Row
+                      for (col = startPos; col <= lastPos - 1; col++, index++) res[index] = matrix[startPos][col];
+                      // get last Col
+                      for (row = startPos; row <= lastPos - 1; row++, index++) res[index] = matrix[row][lastPos];
+                      //get last Row inverted
+                      for (col = lastPos; col >= startPos + 1; col--, index++) res[index] = matrix[lastPos][col];
+                      //get first Col inverted
+                      for (row = lastPos; row >= startPos + 1; row--, index++) res[index] = matrix[row][startPos];
+                      ++startPos;
+                      --lastPos;
+                  }
+                  if (!(N % 2 == 0)) res[index] = matrix[startPos][startPos];   // if N isn't an even number
+                  return res;
+              }
+        };
+        //return future
+        return executorService.submit(callable);
+    }
 
 }
 
